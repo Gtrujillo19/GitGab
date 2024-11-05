e52983b87b4eb4ba80d74c195fb51ef6afb3a48e
“Gtrujillo19” <4btrujillog@gmail.com>
2024-11-05 20:16:40 +0100

    LC

---
+++ DAM/Programación/Proyectos/JavaProyect1/src/module-info.java
@@ -0,0 +1,8 @@
+/**
+ * 
+ */
+/**
+ * 
+ */
+module JavaProyect1 {
+}
---
+++ DAM/Programación/Proyectos/JavaProyect1/src/myjavaproyect1/myjavaproyect12.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package myjavaproyect1;
+
+/**
+ * 
+ */
+public class myjavaproyect12 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
