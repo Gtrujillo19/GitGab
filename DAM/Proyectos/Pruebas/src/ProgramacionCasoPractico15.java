a3a73dc63fee00c73d1ced614415bbdf74b5911b
“Gtrujillo19” <4btrujillog@gmail.com>
2024-12-16 22:28:02 +0100

    lc

--- DAM/Proyectos/Pruebas/src/ProgramacionCasoPractico15.java
+++ DAM/Proyectos/Pruebas/src/ProgramacionCasoPractico15.java
@@ -3,8 +3,8 @@ public class ProgramacionCasoPractico15 {
 
 	public static void main(String[] args) {
 		
-        int contadorMultiplos2 = 0;  // Contador de múltiplos de 2
-        int contadorMultiplos3 = 0;  // Contador de múltiplos de 3
+		int contadorMultiplos2 = 0;  // Contador de múltiplos de 2
+		int contadorMultiplos3 = 0;  // Contador de múltiplos de 3
 
         for (int i = 0; i <= 100; i++) {
 
